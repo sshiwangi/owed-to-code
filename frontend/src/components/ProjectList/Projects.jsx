c5d3234fe730566fc12892aed938ff01fce3273e
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-08-13 14:10:36 +0530

    added files

---
+++ frontend/src/components/ProjectList/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Projects() {
+  return (
+   <div className="project-list">
+    Projects
+    <hr />
+    Drink water
+    <div className="label">Health</div>
+   </div>
+
+  )
+}
+
+export default Projects
--- frontend/src/pages/Task/Task.jsx
+++ frontend/src/pages/Task/Task.jsx
@@ -3,6 +3,7 @@ import './task.css'
 import plusIcon from '../../assets/Plus Math.png'
 import NoProject from '../../components/Noproject/NoProject'
 import Project from '../../components/Project/Project'
+import Projects from '../../components/ProjectList/Projects'
 
 function Task() {
     const [showNewComponent, setShowNewComponent] = useState(false);
@@ -14,9 +15,11 @@ function Task() {
     <div className="task-list-container">
         <div className="sidebar">
             <div onClick={handleButtonClick} className="project-container">
-                Create Project
+                Create Task
                 <img style={{marginLeft: '0.6rem'}} height="30px" src={plusIcon} alt="" />
+            
             </div>
+            <Projects/>
         </div>
         <div className="main-container">
             {showNewComponent ? <Project /> : <NoProject />}
